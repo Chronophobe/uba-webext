fafb7cf33f4171679ae1fb328da88ab318675c3b
Stan Janssen <stanjanssen@outlook.com>
2017-03-01 00:50:05 +0100

    basics

---
+++ background.js
@@ -0,0 +1,29 @@
+function createUrl(original) {
+    parser = document.createElement('a');
+    parser.href = original;
+
+    url = parser.protocol;
+    url += '//';
+    url += parser.hostname;
+    url += '.proxy.uba.uva.nl:2048';
+    url += parser.pathname;
+    url += parser.search;
+    url += parser.hash;
+    return url;
+}
+
+function gotoProxy() {
+    var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
+    gettingActiveTab.then( function(tabs){
+        if(tabs[0]){
+            var tab = tabs[0];
+            console.log(tab.url);
+            console.log(createUrl(tab.url));
+            browser.tabs.create({
+                active: true,
+                url: createUrl(tab.url)
+            })
+        }
+    });
+}
+browser.browserAction.onClicked.addListener(gotoProxy);
